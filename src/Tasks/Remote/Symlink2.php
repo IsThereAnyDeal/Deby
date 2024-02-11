6154e97e34d7b70861a7212ca024f7575682c847
Tomáš Fedor <tomas.fedor3@gmail.com>
2024-02-11 14:40:48 +0100

    Add Symlink2 that allows to set a targetDir

---
+++ src/Tasks/Remote/Symlink2.php
@@ -0,0 +1,39 @@
+<?php
+namespace IsThereAnyDeal\Tools\Deby\Tasks\Remote;
+
+use IsThereAnyDeal\Tools\Deby\Runtime\Runtime;
+use IsThereAnyDeal\Tools\Deby\Tasks\Task;
+use IsThereAnyDeal\Tools\Deby\Tasks\Vars;
+
+/**
+ * Symlink shared files or folders
+ */
+class Symlink2 implements Task
+{
+    /** @var list<string> */
+    private readonly array $files;
+
+    private readonly string $targetDir;
+
+    public function __construct(string $targetDir, string ...$files) {
+        $this->targetDir = $targetDir;
+        $this->files = array_values($files);
+    }
+
+    public function run(Runtime $runtime): void {
+        $ssh = $runtime->getSshClient();
+
+        if (!$runtime->hasVar(Vars::ReleaseName)) {
+            throw new \ErrorException("No release found");
+        }
+
+        $release = $runtime->getVar(Vars::ReleaseName);
+        if (!is_string($release)) {
+            throw new \InvalidArgumentException();
+        }
+
+        foreach($this->files as $file) {
+            $ssh->symlink("%releases%/{$release}/{$file}", "{$this->targetDir}/{$file}");
+        }
+    }
+}
